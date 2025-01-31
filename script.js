257810407a919c206af02908b73bea0749024663
larav1 <l.vitorinha09@gmail.com>
2025-01-31 15:19:29 -0300

    Ajustes no CSS

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //BOM DIA | BOA TARDE | BOA NOITE
 
 // Obtém a referência do elemento com o ID "greeting"
-const greetingElement = document.getElementById("music-greeting");
+const greetingElement = document.getElementById("greeting");
 
 // Obtém a hora atual do sistema
 const currentHour = new Date().getHours();
